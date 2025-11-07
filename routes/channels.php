3b8ece7718adefc0e8720d8028050b59a5af8771
dusannesic817 <dusannesic28@gmail.com>
2025-11-07 15:05:04 +0100

    notification

---
+++ routes/channels.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Support\Facades\Broadcast;
+
+/*
+|--------------------------------------------------------------------------
+| Broadcast Channels
+|--------------------------------------------------------------------------
+|
+| Ovde definišete kanale za broadcast notifikacije.
+|
+*/
+
+Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
+    return (int) $user->id === (int) $id;
+});
